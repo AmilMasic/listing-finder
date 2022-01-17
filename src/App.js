b3dc5a0a0ff86563abc291a1c552be21f4718cce
AmilMasic <amil.masic@gmail.com>
2022-01-17 12:39:39 -0500

    connected Search and listingsData to App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import './App.css';
-
-import ListingSearch from './components/ListingSearch';
 import listingsData from './components/listingsData';
 
+import Search from './components/Search';
+
 function App() {
+
+
   return (
     <div className="App">
+
       <h1> Welcome to ListingSearch!</h1>
       <h3> a SPA that lets users list and search for places that are available currently</h3>
-      < ListingSearch listingsToSearch={listingsData} />
+      <Search details={listingsData} />
     </div>
   );
 }
