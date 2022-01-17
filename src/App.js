42d318ab891d0f6f0f9944ecefa5791b0778283b
AmilMasic <amil.masic@gmail.com>
2022-01-17 11:30:42 -0500

    wired everything up to work again

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 
-import ListingSearch from './components/listing-search';
+import ListingSearch from './components/ListingSearch';
+import listingsData from './components/listingsData';
 
 function App() {
   return (
     <div className="App">
       <h1> Welcome to ListingSearch!</h1>
       <h3> a SPA that lets users list and search for places that are available currently</h3>
-      < ListingSearch />
+      < ListingSearch listingsToSearch={listingsData} />
     </div>
   );
 }
