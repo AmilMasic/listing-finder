0dec7d518e529edcaba4b9ba173b776e1fb11c90
AmilMasic <amil.masic@gmail.com>
2022-01-17 10:10:05 -0500

    added a greeting message to App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import ListingSearch from './components/listing-search';
 function App() {
   return (
     <div className="App">
+      <h1> Welcome to ListingSearch!</h1>
+      <h3> a SPA that lets users list and search for places that are available currently</h3>
       < ListingSearch />
     </div>
   );
