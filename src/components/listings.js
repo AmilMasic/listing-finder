a780af3144dfd45e77325ef4016676261b602088
AmilMasic <amil.masic@gmail.com>
2022-01-17 11:13:39 -0500

    moved listings into seperate file

---
+++ src/components/listings.js
@@ -0,0 +1,109 @@
+const listings = [
+  {
+    id: 1,
+    type: "Townhouse",
+    address: "123 Avenue",
+    city: "San Fran",
+    state: "CA",
+    zipcode: 19876,
+    price: 1280,
+    bedrooms: 2,
+    bathrooms: 1,
+    square_footage: 1200,
+    description: `
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+        nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+        reprehenderit in voluptate velit esse cillum dolore eu fugiat
+        nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+        sunt in culpa qui officia deserunt mollit anim id est laborum.
+      `,
+  },
+  {
+    id: 2,
+    type: "Duplex",
+    address: "123 Elm Street",
+    city: "Austin",
+    state: "TX",
+    zipcode: 19876,
+    price: 1280,
+    bedrooms: 2,
+    bathrooms: 1,
+    square_footage: 1200,
+    description: `
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+        nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+        reprehenderit in voluptate velit esse cillum dolore eu fugiat
+        nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+        sunt in culpa qui officia deserunt mollit anim id est laborum.
+      `,
+  },
+  {
+    id: 3,
+    type: "Single Family",
+    address: "123 Brook Street",
+    city: "Los Angeles",
+    state: "CA",
+    zipcode: 19876,
+    price: 1280,
+    bedrooms: 2,
+    bathrooms: 1,
+    square_footage: 1200,
+    description: `
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+        nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+        reprehenderit in voluptate velit esse cillum dolore eu fugiat
+        nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+        sunt in culpa qui officia deserunt mollit anim id est laborum.
+      `,
+  },
+  {
+    id: 4,
+    type: "Townhouse",
+    address: "Elbow Lane",
+    city: "San Fran",
+    state: "CA",
+    zipcode: 19876,
+    price: 1280,
+    bedrooms: 2,
+    bathrooms: 1,
+    square_footage: 1200,
+    description: `
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+        nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+        reprehenderit in voluptate velit esse cillum dolore eu fugiat
+        nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+        sunt in culpa qui officia deserunt mollit anim id est laborum.
+      `,
+  },
+  {
+    id: 5,
+    type: "Duplex",
+    address: "Street Road",
+    city: "Warrington",
+    state: "PA",
+    zipcode: 19876,
+    price: 1280,
+    bedrooms: 2,
+    bathrooms: 1,
+    square_footage: 1200,
+    description: `
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+        nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+        reprehenderit in voluptate velit esse cillum dolore eu fugiat
+        nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+        sunt in culpa qui officia deserunt mollit anim id est laborum.
+      `,
+  },
+];
+
+export default listings;
