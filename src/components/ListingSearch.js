7abe9c99820ca6618c707c77c1598873bb4864ac
AmilMasic <amil.masic@gmail.com>
2022-01-17 11:30:29 -0500

    refactored ListingSearch.js, moving map function into a constant

--- src/components/ListingSearch.js
+++ src/components/ListingSearch.js
@@ -1,18 +1,21 @@
 import React from 'react';
 
+import DisplayListings from './DisplayListings';
 
-const ListingSearch = () => {
 
+const ListingSearch = ({listingsToSearch}) => {
+  const listings = listingsToSearch.map(
+    listing =>
+      < DisplayListings
+        key={listing.id}
+        listing={listing}
+      />)
 
-  return <div>
-
-          {listings && listings.map(listing =>
-            <div key={listing.id}>
-            
-            </div>
-          )}
-
-        </div>
+  return (
+    <div>
+      {listings}
+    </div>
+  );
 };
 
 export default ListingSearch;
