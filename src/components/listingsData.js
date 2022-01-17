851c192269edbc35d798bcde06620deaea184d93
AmilMasic <amil.masic@gmail.com>
2022-01-17 12:47:36 -0500

    added more dummy data

--- src/components/listingsData.js
+++ src/components/listingsData.js
@@ -3,7 +3,7 @@ const listings = [
     id: 1,
     type: "Townhouse",
     address: "123 Avenue",
-    city: "San Fran",
+    city: "San Francisco",
     state: "CA",
     zipcode: 19876,
     price: 1280,
@@ -66,7 +66,7 @@ const listings = [
     id: 4,
     type: "Townhouse",
     address: "Elbow Lane",
-    city: "San Fran",
+    city: "San Francisco",
     state: "CA",
     zipcode: 19876,
     price: 1280,
@@ -84,11 +84,137 @@ const listings = [
       `,
   },
   {
-    id: 5,
+    id: 6,
     type: "Duplex",
     address: "Street Road",
     city: "Warrington",
     state: "PA",
+    zipcode: 19876,
+    price: 1280,
+    bedrooms: 2,
+    bathrooms: 1,
+    square_footage: 1200,
+    description: `
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+        nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+        reprehenderit in voluptate velit esse cillum dolore eu fugiat
+        nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+        sunt in culpa qui officia deserunt mollit anim id est laborum.
+      `,
+  },
+  {
+    id: 7,
+    type: "Duplex",
+    address: "Street Road",
+    city: "Las Vegas",
+    state: "NV",
+    zipcode: 19876,
+    price: 1280,
+    bedrooms: 2,
+    bathrooms: 1,
+    square_footage: 1200,
+    description: `
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+        nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+        reprehenderit in voluptate velit esse cillum dolore eu fugiat
+        nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+        sunt in culpa qui officia deserunt mollit anim id est laborum.
+      `,
+  },
+  {
+    id: 8,
+    type: "Duplex",
+    address: "Street Road",
+    city: "Washington",
+    state: "DC",
+    zipcode: 19876,
+    price: 1280,
+    bedrooms: 2,
+    bathrooms: 1,
+    square_footage: 1200,
+    description: `
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+        nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+        reprehenderit in voluptate velit esse cillum dolore eu fugiat
+        nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+        sunt in culpa qui officia deserunt mollit anim id est laborum.
+      `,
+  },
+  {
+    id: 9,
+    type: "Duplex",
+    address: "Street Road",
+    city: "Los Angeles",
+    state: "CA",
+    zipcode: 19876,
+    price: 1280,
+    bedrooms: 2,
+    bathrooms: 1,
+    square_footage: 1200,
+    description: `
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+        nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+        reprehenderit in voluptate velit esse cillum dolore eu fugiat
+        nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+        sunt in culpa qui officia deserunt mollit anim id est laborum.
+      `,
+  },
+  {
+    id: 10,
+    type: "Duplex",
+    address: "Street Road",
+    city: "Los Angeles",
+    state: "CA",
+    zipcode: 19876,
+    price: 1280,
+    bedrooms: 2,
+    bathrooms: 1,
+    square_footage: 1200,
+    description: `
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+        nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+        reprehenderit in voluptate velit esse cillum dolore eu fugiat
+        nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+        sunt in culpa qui officia deserunt mollit anim id est laborum.
+      `,
+  },
+  {
+    id: 11,
+    type: "Duplex",
+    address: "Street Road",
+    city: "Los Angeles",
+    state: "CA",
+    zipcode: 19876,
+    price: 1280,
+    bedrooms: 2,
+    bathrooms: 1,
+    square_footage: 1200,
+    description: `
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+        nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+        reprehenderit in voluptate velit esse cillum dolore eu fugiat
+        nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+        sunt in culpa qui officia deserunt mollit anim id est laborum.
+      `,
+  },
+  {
+    id: 12,
+    type: "Duplex",
+    address: "Street Road",
+    city: "Los Angeles",
+    state: "CA",
     zipcode: 19876,
     price: 1280,
     bedrooms: 2,
