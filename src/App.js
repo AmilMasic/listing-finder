b96e5bf341f3ba263f107ec4b6102ba8680ac4ad
AmilMasic <amil.masic@gmail.com>
2022-01-17 09:09:48 -0500

    created listingSearch component and connected it to App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 
+import ListingSearch from './components/listing-search';
+
 function App() {
   return (
     <div className="App">
-  
+      < ListingSearch />
     </div>
   );
 }
---
+++ src/components/listing-search.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const listings = [
+  {
+    type: "Townhouse",
+    address: "123 Avenue",
+    city: "San Fran",
+    price: 1280,
+    bedrooms: 2,
+    bathrooms: 1,
+    sqaure_footage: 1200}
+];
+
+
+const ListingSearch = () => {
+  return <div> It's wired up </div>
+};
+
+export default ListingSearch;
